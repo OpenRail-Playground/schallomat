0b3f026ba7cc1e8122447385b185bca2e21b46a2
Joachim Schirrmacher <joachim.schirrmacher@deutschebahn.com>
2024-10-01 15:32:11 +0200

    feat: day and night toggle

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,7 @@ import AddressList from '../components/AddressList.vue'
 import CircleComponent from '../components/CircleComponent.vue'
 import MachineWizard from '../components/MachineWizard.vue'
 import { storeToRefs } from 'pinia'
-import { useConstructionSiteStore } from '@/stores/constructionSiteStore'
+import { useConstructionSiteStore } from '../stores/constructionSiteStore'
 
 const home = ref([13.3565907, 52.4815294])
 const radius = ref(100)
@@ -16,6 +16,10 @@ const { center } = storeToRefs(useConstructionSiteStore())
 <template>
   <div class="base-layout">
     <main>
+      <label data-icon-before="day" data-icon-variant-before="20-outline" id="day-label">Tagzeitraum</label>
+      <input type="checkbox" role="switch" class="elm-toggle" id="day-night-toggle" />
+      <label class="elm-label" for="day-night-toggle" id="night-label" data-icon-before="night" data-icon-variant-before="20-outline">Nachtzeitraum</label>
+
       <TheMap :center="home">
         <CircleComponent v-if="center" :center="center" :radius="radius" />
       </TheMap>
@@ -48,5 +52,16 @@ aside {
 
 main {
   grid-area: 1 / 2 / 2 / 3;
+}
+
+#day-label {
+  position: relative;
+  top: -0.3rem;
+  display: inline-block;
+  margin-right: 0.5rem;
+}
+
+#night-label {
+  margin-left: 0.5rem;
 }
 </style>
