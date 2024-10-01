167902a80c3a3a371f36650c5bca7a739187f3c6
Joachim Schirrmacher <joachim.schirrmacher@deutschebahn.com>
2024-10-01 03:38:35 +0200

    feat: deploy to GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/schallomat",
   plugins: [
     vue(),
     vueDevTools(),
