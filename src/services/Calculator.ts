96b5a7372c5efb9b9c022b99f44c2c4a95c64f7a
u231516 <ruedi.becker@sbb.ch>
2024-10-03 16:40:54 +0200

    Improve structure of Isophone-Calculation, aligned with Documentation

--- src/services/Calculator.ts
+++ src/services/Calculator.ts
@@ -1,7 +1,14 @@
 import type { Machine } from '../stores/constructionMachineStore'
 import { getImmissionThresholds } from './Isophones'
 
-export const IMMISSION_THRESHOLDS = getImmissionThresholds()
+export const immissionThresholds = getImmissionThresholds()
+
+const sourceHeight = 3.0 // Wir aktuell nicht aus den Stammdaten der Baumaschinen entnommen
+const receiverHeight = 5.1 // Gebäude 1. Stock
+const maxDistance = 100000 // Maximaler zu rechnenden Radius
+const stepDistance = 5 // Granularität der Radiusberechnung
+const coefficientAbsorption = 2.8 // 500Hz, 20°C
+
 
 // Funktion zur Berechnung der Isophonen
 export function calculateIsophones(machines: Machine[]) {
@@ -22,100 +29,137 @@ export function calculateIsophones(machines: Machine[]) {
 
   // Berechnung der Radien basierend auf den Geräuschpegeln
   if (dayNoiseLevels.length !== 0) {
-    dayRadii = calculateRadii(sumNoiseLevels(dayNoiseLevels), IMMISSION_THRESHOLDS.day)
+    dayRadii = calculateRadii(sumNoiseLevels(dayNoiseLevels), immissionThresholds.day, sourceHeight, receiverHeight)
   }
   if (nightNoiseLevels.length !== 0) {
-    nightRadii = calculateRadii(sumNoiseLevels(nightNoiseLevels), IMMISSION_THRESHOLDS.night)
+    nightRadii = calculateRadii(sumNoiseLevels(nightNoiseLevels), immissionThresholds.night, sourceHeight, receiverHeight)
   }
 
   return {
     day: dayRadii,
     night: nightRadii
   }
+}
 
-  // Berechnung des Nachtbonus
-  function calculateNightBonus(duration: number): number {
-    if (duration < 2) {
-      return -10
-    } else if (duration < 6) {
-      return -5
-    }
-    return 0
+// Berechnung des Nachtbonus (Anpassung der Dezibel aufgrund der Lärmdauer)
+function calculateNightBonus(duration: number): number {
+  if (duration < 2) {
+    return -10
+  } else if (duration < 6) {
+    return -5
   }
+  return 0
+}
 
-  // Berechnung des Tagbonus
-  function calculateDayBonus(duration: number): number {
-    if (duration < 2.5) {
-      return -10
-    } else if (duration < 8) {
-      return -5
-    }
-    return 0
+// Berechnung des Tagbonus (Anpassung der Dezibel aufgrund der Lärmdauer)
+function calculateDayBonus(duration: number): number {
+  if (duration < 2.5) {
+    return -10
+  } else if (duration < 8) {
+    return -5
   }
+  return 0
+}
 
-  // Summe der Geräuschpegel berechnen
-  function sumNoiseLevels(noiseLevels: number[]): number {
-    return 10 * Math.log10(noiseLevels.reduce((sum, level) => sum + 10 ** (level / 10), 0))
-  }
+// Summe der Geräuschpegel berechnen
+function sumNoiseLevels(noiseLevels: number[]): number {
+  return 10 * Math.log10(noiseLevels.reduce((sum, level) => sum + 10 ** (level / 10), 0))
+}
 
-  // Funktion zur Berechnung der Radien
-  function calculateRadii(
-    totalNoiseLevel: number,
-    immissionThresholds: number[],
-    sourceHeight = 3.0
-  ) {
-    const radii: Record<number, number> = {}
+// Funktion zur Berechnung der Radien
+function calculateRadii(
+  totalNoiseLevel: number,
+  immissionThresholds: number[],
+  sourceHeight: number,
+  receiverHeight: number,
+) {
+  const radii: Record<number, number> = {}
 
-    for (let distance = 5; distance <= 100000; distance += 5) {
-      const soundPressure = calculateSoundPressure(totalNoiseLevel, distance, sourceHeight)
+  for (let distance = stepDistance; distance <= maxDistance; distance += stepDistance) {
+    const soundPressure = calculateSoundPressure(totalNoiseLevel, distance, sourceHeight, receiverHeight)
 
-      for (let i = 0; i < immissionThresholds.length; i++) {
-        if (soundPressure >= immissionThresholds[i]) {
-          radii[immissionThresholds[i]] = distance
-        }
+    immissionThresholds.forEach(threshold => {
+      if (soundPressure >= threshold) {
+        radii[threshold] = distance;
       }
+    })
 
-      if (soundPressure < Math.min(...immissionThresholds)) {
-        break
-      }
+    if (soundPressure < Math.min(...immissionThresholds)) {
+      break
     }
-
-    return radii
   }
 
-  // Berechnung des Schalldrucks
-  function calculateSoundPressure(
-    noiseLevel: number,
-    distance: number,
-    sourceHeight: number
-  ): number {
-    const heightDifferenceMinus = sourceHeight - 5.1
-    const heightDifferencePlus = sourceHeight + 5.1
-    const distanceSquared = distance ** 2
-    return (
-      noiseLevel +
-      calculateDirectionalCorrection(distanceSquared, heightDifferenceMinus, heightDifferencePlus) -
-      (20 * Math.log10(distance) +
-        11 +
-        (2.8 * distance) / 1000 +
-        4.8 -
-        (heightDifferencePlus / distance) * (17 + 300 / distance))
+  return radii
+}
+
+// Berechnung des Schalldrucks
+function calculateSoundPressure(
+  noiseLevel: number,
+  distance: number,
+  sourceHeight: number,
+  receiverHeight: number,
+): number {
+  return (
+    noiseLevel +
+    calculateDirectionalCorrection(distance, sourceHeight, receiverHeight) -
+    calculateOctaveBandAbsorption(distance, sourceHeight, receiverHeight)
+  )
+}
+
+// Berechnung Oktavbanddämpfung
+function calculateOctaveBandAbsorption(
+  distance: number,
+  sourceHeight: number,
+  receiverHeight: number,
+): number {
+  return (
+    calculateGeometricDiffusion(distance) +
+    calculateAirAbsorption(distance) +
+    calculateGroundAbsorption(distance, sourceHeight, receiverHeight)
+  )
+}
+
+// Berechnung der Bodendämpfung
+function calculateGroundAbsorption(
+  distance: number,
+  sourceHeight: number,
+  receiverHeight: number,
+): number {
+  return (
+    4.8 - ((sourceHeight + receiverHeight) / distance) * (17 + 300 / distance)
+  )
+}
+
+// Berechnung der geometrischen Ausbreitung
+function calculateGeometricDiffusion(
+  distance: number,
+): number {
+  return (
+    20 * Math.log10(distance) + 11
+  )
+}
+
+// Berechnung der Luftabsorption
+function calculateAirAbsorption(
+  distance: number,
+): number {
+  return (
+    coefficientAbsorption * distance / 1000
+  )
+}
+
+// Berechnung der Richtwirkungskorrektur
+function calculateDirectionalCorrection(
+  distance: number,
+  sourceHeight: number,
+  receiverHeight: number,
+) {
+  return (
+    10 *
+    Math.log10(
+      1 +
+        ((distance ** 2) + sourceHeight - (receiverHeight ** 2)) /
+          ((distance ** 2) + sourceHeight + (receiverHeight ** 2))
     )
-  }
-
-  // Berechnung der Richtwirkungskorrektur
-  function calculateDirectionalCorrection(
-    distanceSquared: number,
-    heightDifferenceMinus: number,
-    heightDifferencePlus: number
-  ) {
-    return (
-      10 *
-      Math.log10(
-        1 +
-          (distanceSquared + heightDifferenceMinus ** 2) /
-            (distanceSquared + heightDifferencePlus ** 2)
-      )
-    )
-  }
+  )
 }
